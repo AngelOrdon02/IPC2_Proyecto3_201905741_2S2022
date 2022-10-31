bfdf3a5b7c28f0e00e6596a9a4aadf9fe3c35a3d
AngelOrdon02 <3006613200101@ingenieria.usac.edu.gt>
2022-10-31 02:03:28 -0700

    Login and Logout

--- FrontEnd/webapp/frontend/views.py
+++ FrontEnd/webapp/frontend/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import requests
 
 import json
+
+from django.contrib import messages
 
 # Home
 def home(request):
@@ -10,6 +12,35 @@ def home(request):
 
 # Login
 def login_view(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        # print("Data: " + username + ", " + password)
+
+        if (username != None and password != None):
+            params = {'username': username, 'password': password}
+            response = requests.post('http://127.0.0.1:3000/login', json = params)
+            
+            res = response.json()
+            #print("respuesta: " + res['message'] + ", " + str(res['state']))
+
+            if (res['state'] == 1):
+                request.session['session_id'] = str(res['id'])
+                request.session['session_username'] = str(username)
+                return redirect('users')
+            elif (res['state'] == 2):
+                messages.info(request, 'Contraseña incorrecta')
+                return render(request, "auth/loginView.html", {})
+                #return HttpResponse("""your form is wrong, reload on <a href = "">reload</a>""")
+            elif (res['state'] == 3):
+                messages.info(request, 'Nombre de usuario incorrecto')
+                return render(request, "auth/loginView.html", {})
+            elif (res['state'] == 4):
+                messages.info(request, 'Nombre de usuario y contraseña incorrectos')
+                return render(request, "auth/loginView.html", {})
+
     return render(request, "auth/loginView.html", {})
 
 # Create your views here.
